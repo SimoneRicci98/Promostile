95a9ef5587b33338b31f94150def053e11402d3a
SimoneRicci98 <ricci.simone98@gmail.com>
2017-10-03 11:19:05 +0200

    Added form for add items
    not working, just added

---
+++ GestisciProdotti.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="GestisciProdotti.aspx.cs" Inherits="GestisciProdotti" %>
+
+<asp:Content ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div class="col-xs-12">
+        Aggiungi qui i nuovi prodotti
+   <asp:GridView ID="grvProdotti" runat="server" AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" GridLines="None" OnRowDeleting="grvStudentDetails_RowDeleting" ShowFooter="True" Style="text-align: left" Width="95%" >
+                   <Columns>
+                       <asp:BoundField DataField="RowNumber" HeaderText="Nr." />
+                          <asp:TemplateField HeaderText="Codice">
+                           <ItemTemplate>
+                               <asp:TextBox Width="80%" ID="txtCodice" runat="server" ></asp:TextBox>
+                           </ItemTemplate>
+                       </asp:TemplateField>
+                       <asp:TemplateField HeaderText="Descrizione">
+                           <ItemTemplate>
+                               <asp:TextBox Width="80%" ID="txtDesc" runat="server" ></asp:TextBox>
+                           </ItemTemplate>
+                       </asp:TemplateField>
+                       <asp:TemplateField HeaderText="Prezzo">
+                           <ItemTemplate>
+                               <asp:TextBox Width="80%" ID="txtPrezzo" runat="server" ></asp:TextBox>
+                           </ItemTemplate>
+                       </asp:TemplateField>
+                       <asp:TemplateField HeaderText="Quantità">
+                           <ItemTemplate>
+                               <asp:TextBox Width="80%" ID="txtQta" runat="server"></asp:TextBox>
+                           </ItemTemplate>
+                           <FooterStyle HorizontalAlign="Right" />
+                           <FooterTemplate>
+                               <asp:Button ID="ButtonAdd" CssClass="btn btn-primary" runat="server" OnClick="ButtonAdd_Click" Text="Aggiungi una riga" />
+                           </FooterTemplate>
+                       </asp:TemplateField>
+                       <asp:TemplateField HeaderText="Marca">
+                           <ItemTemplate>
+                               <asp:TextBox Width="80%" ID="txtMarca" runat="server" ></asp:TextBox>
+                           </ItemTemplate>
+                       </asp:TemplateField>
+                       <asp:CommandField ShowDeleteButton="True" />
+                   </Columns>
+                   <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                   <RowStyle BackColor="#EFF3FB" />
+                   <EditRowStyle BackColor="#2461BF" />
+                   <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                   <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                   <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                   <AlternatingRowStyle BackColor="White" />
+               </asp:GridView>
+            <br /><br />
+        <asp:Button ID="btnSave" runat="server" CssClass="btn btn-primary" OnClick="btnSave_Click" Text="Salva dati" />
+    </div>
+    <div class="col-xs-5" style="margin-top:2%">
+      <h4> Seleziona qui il prodotto da gestire <asp:DropDownList ID="drpProdotti" runat="server" Width="40%" DataSourceID="SqlDataSource1" DataTextField="Descrizione" DataValueField="Descrizione"></asp:DropDownList>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ContabilitàDBConnectionString %>" SelectCommand="SELECT [Descrizione] FROM [Prodotti] WHERE ([COD_Azienda] = @COD_Azienda)">
+            <SelectParameters>
+                <asp:SessionParameter Name="COD_Azienda" SessionField="Azienda" Type="String" />
+            </SelectParameters>
+        </asp:SqlDataSource>
+          </h4>
+    </div>
+    <div class="col-xs-12" style="margin-top:2%">
+    <div class="col-xs-4">
+        Aggiungi quantità <asp:TextBox ID="txtAgg" runat="server" TextMode="Number"></asp:TextBox><br /><br />
+        <asp:Button ID="btnAgg" CssClass="btn btn-primary" runat="server" Width="100%" Text="Aggiungi" OnClick="btnAgg_Click" />
+    </div>
+    <div class="col-xs-4">
+        Modifica prezzo <asp:TextBox ID="txtPrez" runat="server" TextMode="Number"></asp:TextBox><br /><br />
+        <asp:Button ID="btnPrez" CssClass="btn btn-primary" runat="server" Width="100%" Text="Modifica" OnClick="btnPrez_Click" />
+    </div>
+    <div class="col-xs-4">
+        <asp:Button ID="btnElimina" CssClass="btn btn-primary" runat="server" Width="100%" Text="Elimina prodotto" OnClick="btnElimina_Click" />
+    </div>
+    </div>   
+</asp:Content>
---
+++ GestisciProdotti.aspx.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class GestisciProdotti : System.Web.UI.Page
+{
+    dbHelper help = new dbHelper();
+    SqlDataReader rs;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            FirstGridViewRow();
+        }
+    }
+
+    private void FirstGridViewRow()
+    {
+        DataTable dt = new DataTable();
+        DataRow dr = null;
+        dt.Columns.Add(new DataColumn("RowNumber", typeof(string)));
+        dt.Columns.Add(new DataColumn("Col1", typeof(string)));
+        dt.Columns.Add(new DataColumn("Col2", typeof(string)));
+        dt.Columns.Add(new DataColumn("Col3", typeof(string)));
+        dt.Columns.Add(new DataColumn("Col4", typeof(string)));
+        dt.Columns.Add(new DataColumn("Col5", typeof(string)));
+        dr = dt.NewRow();
+        dr["RowNumber"] = 1;
+        dr["Col1"] = string.Empty;
+        dr["Col2"] = string.Empty;
+        dr["Col3"] = string.Empty;
+        dr["Col4"] = string.Empty;
+        dr["Col5"] = string.Empty;
+        dt.Rows.Add(dr);
+
+        ViewState["CurrentTable"] = dt;
+
+
+        grvProdotti.DataSource = dt;
+        grvProdotti.DataBind();
+
+        Button btnAdd = (Button)grvProdotti.FooterRow.Cells[2].FindControl("ButtonAdd");
+        Page.Form.DefaultFocus = btnAdd.ClientID;
+
+    }
+    private void AddNewRow()
+    {
+        int rowIndex = 0;
+
+        if (ViewState["CurrentTable"] != null)
+        {
+            DataTable dtCurrentTable = (DataTable)ViewState["CurrentTable"];
+            DataRow drCurrentRow = null;
+            if (dtCurrentTable.Rows.Count > 0)
+            {
+                for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                {
+                    TextBox Codice = (TextBox)grvProdotti.Rows[rowIndex].Cells[1].FindControl("txtCodice");
+                    TextBox Descrizione = (TextBox)grvProdotti.Rows[rowIndex].Cells[2].FindControl("txtDesc");
+                    TextBox Prezzo = (TextBox)grvProdotti.Rows[rowIndex].Cells[3].FindControl("txtPrezzo");
+                    TextBox Qta = (TextBox)grvProdotti.Rows[rowIndex].Cells[4].FindControl("txtQta");
+                    TextBox Marca = (TextBox)grvProdotti.Rows[rowIndex].Cells[5].FindControl("txtMarca");
+
+                    drCurrentRow = dtCurrentTable.NewRow();
+                    drCurrentRow["RowNumber"] = i + 1;
+
+                    dtCurrentTable.Rows[i - 1]["Col1"] = Codice.Text;
+                    dtCurrentTable.Rows[i - 1]["Col2"] = Descrizione.Text;
+                    dtCurrentTable.Rows[i - 1]["Col3"] = Prezzo.Text;
+                    dtCurrentTable.Rows[i - 1]["Col4"] = Qta.Text;
+                    dtCurrentTable.Rows[i - 1]["Col5"] = Marca.Text;
+                    rowIndex++;
+                }
+                dtCurrentTable.Rows.Add(drCurrentRow);
+                ViewState["CurrentTable"] = dtCurrentTable;
+
+                grvProdotti.DataSource = dtCurrentTable;
+                grvProdotti.DataBind();
+            }
+        }
+        else
+        {
+            Response.Write("ViewState is null");
+        }
+        SetPreviousData();
+    }
+    private void SetPreviousData()
+    {
+
+        int rowIndex = 0;
+        if (ViewState["CurrentTable"] != null)
+        {
+            DataTable dt = (DataTable)ViewState["CurrentTable"];
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    ;
+                    TextBox Codice = (TextBox)grvProdotti.Rows[rowIndex].Cells[1].FindControl("txtCodice");
+                    TextBox Descrizione = (TextBox)grvProdotti.Rows[rowIndex].Cells[2].FindControl("txtDesc");
+                    TextBox Prezzo = (TextBox)grvProdotti.Rows[rowIndex].Cells[3].FindControl("txtPrezzo");
+                    TextBox Qta = (TextBox)grvProdotti.Rows[rowIndex].Cells[4].FindControl("txtQta");
+                    TextBox Marca = (TextBox)grvProdotti.Rows[rowIndex].Cells[4].FindControl("txtMarca");
+
+                    grvProdotti.Rows[i].Cells[0].Text = Convert.ToString(i + 1);
+                    Codice.Text = dt.Rows[i]["Col1"].ToString();
+                    Descrizione.Text = dt.Rows[i]["Col2"].ToString();
+                    Prezzo.Text = dt.Rows[i]["Col3"].ToString();
+                    Qta.Text = dt.Rows[i]["Col4"].ToString();
+                    Marca.Text = dt.Rows[i]["Col5"].ToString();
+                    rowIndex++;
+                }
+            }
+        }
+    }
+    protected void ButtonAdd_Click(object sender, EventArgs e)
+    {
+        AddNewRow();
+    }
+    protected void grvStudentDetails_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        SetRowData();
+        if (ViewState["CurrentTable"] != null)
+        {
+            DataTable dt = (DataTable)ViewState["CurrentTable"];
+            DataRow drCurrentRow = null;
+            int rowIndex = Convert.ToInt32(e.RowIndex);
+            if (dt.Rows.Count > 1)
+            {
+                dt.Rows.Remove(dt.Rows[rowIndex]);
+                drCurrentRow = dt.NewRow();
+                ViewState["CurrentTable"] = dt;
+                grvProdotti.DataSource = dt;
+                grvProdotti.DataBind();
+
+                for (int i = 0; i < grvProdotti.Rows.Count - 1; i++)
+                {
+                    grvProdotti.Rows[i].Cells[0].Text = Convert.ToString(i + 1);
+                }
+                SetPreviousData();
+            }
+        }
+    }
+
+    private void SetRowData()
+    {
+        int rowIndex = 0;
+
+        if (ViewState["CurrentTable"] != null)
+        {
+            DataTable dtCurrentTable = (DataTable)ViewState["CurrentTable"];
+            DataRow drCurrentRow = null;
+            if (dtCurrentTable.Rows.Count > 0)
+            {
+                for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                {
+                    TextBox Codice = (TextBox)grvProdotti.Rows[rowIndex].Cells[1].FindControl("txtCodice");
+                    TextBox Descrizione = (TextBox)grvProdotti.Rows[rowIndex].Cells[2].FindControl("txtDesc");
+                    TextBox Prezzo = (TextBox)grvProdotti.Rows[rowIndex].Cells[3].FindControl("txtPrezzo");
+                    TextBox Qta = (TextBox)grvProdotti.Rows[rowIndex].Cells[4].FindControl("txtQta");
+                    TextBox Marca = (TextBox)grvProdotti.Rows[rowIndex].Cells[5].FindControl("txtMarca");
+                    drCurrentRow = dtCurrentTable.NewRow();
+                    drCurrentRow["RowNumber"] = i + 1;
+
+                    dtCurrentTable.Rows[i - 1]["Col1"] = Codice.Text;
+                    dtCurrentTable.Rows[i - 1]["Col2"] = Descrizione.Text;
+                    dtCurrentTable.Rows[i - 1]["Col3"] = Prezzo.Text;
+                    dtCurrentTable.Rows[i - 1]["Col4"] = Qta.Text;
+                    dtCurrentTable.Rows[i - 1]["Col5"] = Qta.Text;
+                    rowIndex++;
+                }
+
+                ViewState["CurrentTable"] = dtCurrentTable;
+            }
+        }
+        else
+        {
+            Response.Write("ViewState is null");
+        }
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            SetRowData();
+            DataTable table = ViewState["CurrentTable"] as DataTable;
+
+            if (table != null)
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    string Codice = row.ItemArray[1] as string;
+                    string Descrizione = row.ItemArray[2] as string;
+                    string Prezzo = row.ItemArray[3] as string;
+                    string Qta = row.ItemArray[4] as string;
+                    string Marca = row.ItemArray[5] as string;
+
+                    help.connetti();
+                    help.assegnaComando("INSERT INTO Prodotti(ID_Prodotto,Codice,Descrizione,Prezzo,Qta,Marca) VALUES('" + Codice + "','" + Descrizione + "'," + Prezzo + "," + Qta + ",'" + Marca + "')");
+                    help.eseguicomando();
+                    help.disconnetti();
+                }
+                Response.Redirect(Request.Url.AbsoluteUri);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    protected void btnAgg_Click(object sender, EventArgs e)
+    {
+        help.connetti();
+        help.assegnaComando("SELECT ID FROM Prodotti WHERE Descrizione ='" + drpProdotti.SelectedItem.Text + "'AND COD_Azienda ='" + Session["Azienda"].ToString() + "'");
+        rs = help.estraiDati();
+        rs.Read();
+        string app = rs["ID"].ToString();
+        help.disconnetti();
+
+        help.connetti();
+        help.assegnaComando("UPDATE Prodotti SET Qta=(SELECT Qta FROM Prodotti WHERE Descrizione ='" + drpProdotti.SelectedItem.Text + "' AND COD_Azienda ='" + Session["Azienda"].ToString() + "')+" + txtAgg.Text + " WHERE ID=" + app);
+        help.eseguicomando();
+        help.disconnetti();
+        txtAgg.Text = "";
+        Response.Redirect(Request.Url.AbsoluteUri);
+    }
+
+    protected void btnPrez_Click(object sender, EventArgs e)
+    {
+        help.connetti();
+        help.assegnaComando("SELECT ID FROM Prodotti WHERE Descrizione ='" + drpProdotti.SelectedItem.Text + "'AND COD_Azienda ='" + Session["Azienda"].ToString() + "'");
+        rs = help.estraiDati();
+        rs.Read();
+        string app = rs["ID"].ToString();
+        help.disconnetti();
+
+        help.connetti();
+        help.assegnaComando("UPDATE Prodotti SET Prezzo=" + txtPrez.Text + " WHERE ID=" + app);
+        help.eseguicomando();
+        help.disconnetti();
+        txtPrez.Text = "";
+        Response.Redirect(Request.Url.AbsoluteUri);
+    }
+
+    protected void btnElimina_Click(object sender, EventArgs e)
+    {
+        help.connetti();
+        help.assegnaComando("SELECT ID FROM Prodotti WHERE Descrizione ='" + drpProdotti.SelectedItem.Text + "'AND COD_Azienda ='" + Session["Azienda"].ToString() + "'");
+        rs = help.estraiDati();
+        rs.Read();
+        string app = rs["ID"].ToString();
+        help.disconnetti();
+
+        help.connetti();
+        help.assegnaComando("DELETE Prodotti WHERE ID=" + app);
+        help.eseguicomando();
+        help.disconnetti();
+        Response.Redirect(Request.Url.AbsoluteUri);
+    }
+}
